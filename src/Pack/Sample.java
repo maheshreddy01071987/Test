6305c37ff24b601c7a3aab95b046e7a7aefe8ad0
e5610130 <e5610130@BLR1-LT1Q0T6S2.mshome.net>
2020-09-26 11:45:52 +0530

    commiting new file

--- src/Pack/Sample.java
+++ src/Pack/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("checking commit");
+		System.out.println("checking commit...");
 
 	}
 
